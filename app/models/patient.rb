9d35c147c535b79dd97726ccc55c50cfd9b52adc
github usernameIfeAkiWad <github email addressifeoluwa.wade@gmail.com>
2020-09-25 16:27:41 -0500

    calidating DPatient model

--- app/models/patient.rb
+++ app/models/patient.rb
@@ -1,5 +1,9 @@
 class Patient < ApplicationRecord
     has_many :reviews
     has_many :doctors, through: :reviews
-
+    validates :username, :email, presence: true, uniqueness: true
+    validates :name, presence: true, uniqueness: { case_sensitive: false }
+    validates :password, presence: true
+     #should I validate the presence of location?
+    has_secure_password
 end
