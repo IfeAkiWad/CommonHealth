947ac3e88748504e0d60ff7e74a9335d42f8a4ce
github usernameIfeAkiWad <github email addressifeoluwa.wade@gmail.com>
2020-09-25 16:30:21 -0500

    validating Patient model

--- app/models/patient.rb
+++ app/models/patient.rb
@@ -4,6 +4,6 @@ class Patient < ApplicationRecord
     validates :username, :email, presence: true, uniqueness: true
     validates :name, presence: true, uniqueness: { case_sensitive: false }
     validates :password, presence: true
-     #should I validate the presence of location?
+     #should I validate the presence of location
     has_secure_password
 end
