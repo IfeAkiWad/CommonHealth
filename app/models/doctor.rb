a4dff567b2269cca375bfa5436c8bd580a0a613a
github usernameIfeAkiWad <github email addressifeoluwa.wade@gmail.com>
2020-09-25 16:09:37 -0500

    cvalidating Doctor model

--- app/models/doctor.rb
+++ app/models/doctor.rb
@@ -1,4 +1,8 @@
 class Doctor < ApplicationRecord
     has_many :reviews
     has_many :patients, through: :reviews
+    validates :name, uniqueness: true
+    validates :name, presence: true
+    #should I validate the presence of location, sspecialty, and insurance?
+    
 end
