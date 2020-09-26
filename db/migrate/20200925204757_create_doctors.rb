42cfd69b1ce7a5e7ed6ad75bbff14ea74d692281
github usernameIfeAkiWad <github email addressifeoluwa.wade@gmail.com>
2020-09-25 19:55:43 -0500

    running migrations

--- db/migrate/20200925204757_create_doctors.rb
+++ db/migrate/20200925204757_create_doctors.rb
@@ -1,4 +1,4 @@
-class CreateDoctorsTable < ActiveRecord::Migration[6.0]
+class CreateDoctors < ActiveRecord::Migration[6.0]
   def change
     create_table :doctors do |t|
       t.string :name
--- db/migrate/20200925204935_create_reviews.rb
+++ db/migrate/20200925204935_create_reviews.rb
@@ -1,4 +1,4 @@
-class CreateReviewsTable < ActiveRecord::Migration[6.0]
+class CreateReviews < ActiveRecord::Migration[6.0]
   def change
     create_table :reviews do |t|
       t.integer :doctor_id
