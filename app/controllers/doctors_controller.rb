948c7a694a619a04d344d0c0936720cf70158c98
github usernameIfeAkiWad <github email addressifeoluwa.wade@gmail.com>
2020-09-27 19:44:07 -0500

    building doctors controller

--- app/controllers/doctors_controller.rb
+++ app/controllers/doctors_controller.rb
@@ -1,2 +1,15 @@
 class DoctorsController < ApplicationController
+    def index
+        @doctors = Doctor.all
+    end
+
+    def show
+        @doctor = Doctor.find_by_id(params[:id])
+    end
+
+    # def create
+    # end
+
+    #should there be a create action if there the doctors are already provided in the system?
+    #maybe give doctor oppurtunity to be a user as well?
 end
