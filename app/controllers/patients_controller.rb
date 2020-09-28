2e5abaa236e5f4eb95ee2c629c74e41add11603c
github usernameIfeAkiWad <github email addressifeoluwa.wade@gmail.com>
2020-09-27 20:18:36 -0500

    building patients controller

--- app/controllers/patients_controller.rb
+++ app/controllers/patients_controller.rb
@@ -1,2 +1,45 @@
 class PatientsController < ApplicationController
+
+    def new
+    end
+
+    def index
+        #is an index action necessary?
+    end
+
+    def show
+        patient_set
+    end
+
+    def create
+        @patient = Patient.new(patient_params)
+        if @patient.valid?
+            @patient.save
+            redirect_to patient_path(@patient)
+        else
+            #render :new
+        end
+    end
+
+    def edit
+        patient_set
+    end
+
+    def update
+        patient_set
+        @patient.update(patient_params)
+        redirect_to patietn_path(@patient)
+    end
+
+    def destroy
+    end
+
+    private
+    def patient_set
+        @patient = Patient.find_by_id(params[:id])
+    end
+
+    def patient_params
+        params.require(:patient).permit(:name, :age, :location, :password_confirmation)
+    end
 end
