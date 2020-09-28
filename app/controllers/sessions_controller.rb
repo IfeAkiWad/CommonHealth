08fd382839fff8a0dbb8429c6117b14056cffaa0
github usernameIfeAkiWad <github email addressifeoluwa.wade@gmail.com>
2020-09-27 20:40:03 -0500

    building sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,9 +3,13 @@ class SessionsController < ApplicationController
 
     end
     
-    def create
-        @patient = Patient.find_by(name: params[:patient][:name])
-        return head(:forbidden) unless @user.authenticate(params[:patient][:password])
-        session[:user_id] = @user.id
-      end
-end]
+    # def create
+    #     @patient = Patient.find_by(name: params[:patient][:name])
+    #     return head(:forbidden) unless @user.authenticate(params[:patient][:password])
+    #     session[:user_id] = @user.id
+    # end
+
+    def destroy
+      session.delete :username
+    end
+end
