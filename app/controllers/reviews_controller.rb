e57cb521ba5cbbe798c920f98b13e98beb00904a
github usernameIfeAkiWad <github email addressifeoluwa.wade@gmail.com>
2020-09-27 21:58:39 -0500

    updating reviews controller

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,37 @@
 class ReviewsController < ApplicationController
+    def new
+        @review = Review.new
+    end
+
+    def index
+        @reviews = Review.all
+    end
+
+    def show
+        review_set
+    end
+
+    def create
+    end
+
+    def edit
+        review_set
+    end
+
+    def update
+        review_set
+        @review.update(review_params)
+        redirect_to review_path(@review)
+    end
+
+    private
+    def review_set
+        @review = Review.find_by_id(params[:id])
+    end
+
+    def review_params
+        params.require(:review).permit(:patient_review, :doctor_name, :patient_name)
+    end
+
+
 end
