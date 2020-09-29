c768f5c665b454076d219d6f2842c1619b7e3a92
github usernameIfeAkiWad <github email addressifeoluwa.wade@gmail.com>
2020-09-28 20:06:18 -0500

    incremental update: patients#_form

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
  resources :doctors, only: [:show, :index]
-  resources :patients
+  resources :patients, except: [:index]
  resources :reviews
 
   resources :doctors do
