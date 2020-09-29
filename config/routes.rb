9e4a03d6b53e10ae9b56c0b6e11f1b443acdb5fc
github usernameIfeAkiWad <github email addressifeoluwa.wade@gmail.com>
2020-09-28 19:16:53 -0500

    incremental updates

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
- resources :doctors
- resources :patients
+ resources :doctors, only: [:show, :index]
+  resources :patients
  resources :reviews
+
+  resources :doctors do
+    resources :reviews
+  end
 end
