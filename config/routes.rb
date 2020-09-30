01faf025cd36ce6dfccbc34788b7cb152d41544f
github usernameIfeAkiWad <github email addressifeoluwa.wade@gmail.com>
2020-09-30 16:33:21 -0500

    updating the route file: nested routes, Paths and URLs

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,15 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  #sessions login and sign up
   get 'signup', to: 'users#new'
-  get 'login', to: “sessions#create”
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
  
   resources :doctors, only: [:show, :index]
-  resources :users
- resources :reviews
+  resources :users, only: [:show]
+ resources :reviews, only: [:edit, :update, :destroy]
 
   resources :doctors do
-    resources :reviews
+    resources :reviews, only: [:index, :new, :create]
   end
 end
