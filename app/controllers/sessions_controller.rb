02dbd79126f34ad63364817eaabd9a28db145fc3
github usernameIfeAkiWad <github email addressifeoluwa.wade@gmail.com>
2020-09-30 16:04:01 -0500

    updating sessions controller: user authentication and routing

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,12 +4,13 @@ class SessionsController < ApplicationController
     end
   
   def create
-    @user = User.find_by(username: params[:username])
-    return head(:forbidden) unless @user.authenticate(params[:password])
-    session[:user_id] = @user.id
+    user = User.find_by(username: params[:username])
+    if user && user.authenticate(params[:password]) #if this is true, then
+      session[:user_id] = user.id #user's ID is stored as the value of session[:user_id].
+    redirect_to
   end
 
   def destroy
-    session.delete :username
+    session.clear
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
- resources :doctors, only: [:show, :index]
+  get 'signup', to: 'users#new'
+  get 'login', to: “sessions#create”
+ 
+  resources :doctors, only: [:show, :index]
   resources :users
  resources :reviews
 
