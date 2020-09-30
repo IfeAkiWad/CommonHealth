164b80f480afc1582fc804338100fb68bfcdb61a
github usernameIfeAkiWad <github email addressifeoluwa.wade@gmail.com>
2020-09-30 15:17:49 -0500

    updating user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
     has_many :reviews
     has_many :doctors, through: :reviews
     validates :username, :email, presence: true, uniqueness: true
-    validates :name, presence: true, uniqueness: { case_sensitive: false }
+    validates :name, presence: true
     validates :password, presence: true
     validates :location, presence: true
      #should I validate the presence of location
