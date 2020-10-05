86f285e89f24df3a1c35e8abba55d3d05d9a9bc0
github usernameIfeAkiWad <github email addressifeoluwa.wade@gmail.com>
2020-10-04 20:00:25 -0500

    fixed Doctor model/table error

--- app/models/doctor.rb
+++ app/models/doctor.rb
@@ -2,7 +2,7 @@ class Doctor < ApplicationRecord
     has_many :reviews
     has_many :users, through: :reviews
     # validates :name, uniqueness: true
-    validates :name, :location, :specialty, :insurance, presence: true
+    validates :name, :location, :specialty, :insurance_accepted, presence: true
     #should I validate the presence of location, sspecialty, and insurance?
     
 end
