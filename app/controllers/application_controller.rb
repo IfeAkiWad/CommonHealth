c45b77df955f4cffb74820b086b1cf2c553e498d
github usernameIfeAkiWad <github email addressifeoluwa.wade@gmail.com>
2020-10-06 23:23:45 -0500

    incremental updates: building omniauth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,8 @@ class ApplicationController < ActionController::Base
     
     private
     def current_user
-        user ||= User.find_by_id(session[:user_id])
+        if session[:user_id]
+            @current_user ||= User.find_by_id(session[:user_id])
+        end
     end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -11,7 +11,7 @@ class User < ApplicationRecord
     #method will user object found, or return user object created
     def self.from_omniauth(auth)
         find_or_create_by(username: auth[:info][:email]) do |user|
-            user.password = secureRandom.hex(15)
+            user.password = SecureRandom.hex(15)
         end
     end
 end
