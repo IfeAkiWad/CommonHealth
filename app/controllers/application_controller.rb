c7fd9aaa3b78db2d3a539baa79c2bbfed7693ae1
github usernameIfeAkiWad <github email addressifeoluwa.wade@gmail.com>
2021-03-29 14:14:04 -0500

    created hidden conditional: Doctor search

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,4 +5,14 @@ class ApplicationController < ActionController::Base
             @current_user ||= User.find_by_id(session[:user_id])
         end
     end 
+
+    helper_method :location_specialty
+    def location_specialty
+        @doctors = Doctor.all
+        if !params[:location].blank?
+            @doctors = Doctor.by_location(params[:location]) 
+        elsif !params[:specialty].blank?
+            @doctors = Doctor.by_specialty(params[:specialty])
+        end
+    end
 end
